9807d8f5491500121303fabce0ba7b1812db9308
gansam_7979 <ganesh.samgir@whitecode.co.in>
2025-09-29 23:34:56 +0530

    changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,12 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-      },
-    },
-  },
+  build: {
+    outDir: "dist"
+  }
 });
